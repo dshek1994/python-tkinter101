22e0cc646276f1ef6ddbcd50728f8d0dade392c7
Daniel S <dshekari@icloud.com>
2021-10-04 02:03:46 -0500

    Created a label and a window

--- tkinter101.py
+++ tkinter101.py
@@ -2,4 +2,15 @@ import tkinter as tk
 #Practicing with python gui tkinter
 
 window = tk.Tk()
+greeting = tk.Label(
+    text="Hello, Tkinter",
+    foreground="white",
+    background="black",
+    width=10,
+    height=10
+    )
+
+greeting.pack()
+
+window.mainloop()
 
